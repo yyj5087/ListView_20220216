c36be7a90bde6c6474e3190fc2134b0fcb079f6f
이 영 종 <naki5087@naver.com>
2022-02-16 16:03:45 +0900

    어댑터를 실제로 만들어서 > 리스트뷰와 연결

--- app/src/main/java/com/nepplus/listview_20220216/MainActivity.kt
+++ app/src/main/java/com/nepplus/listview_20220216/MainActivity.kt
@@ -2,10 +2,14 @@ package com.nepplus.listview_20220216
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.nepplus.listview_20220216.adapters.StudentAdapter
 import com.nepplus.listview_20220216.datas.studentData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mAdapter: StudentAdapter
     val mStudentList = ArrayList<studentData>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,14 +18,18 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add(studentData("조경진", 1988,"서울시 동대문구"))
         mStudentList.add(studentData("고재천", 1988,"서울시 중랑구"))
         mStudentList.add(studentData("방우진", 1983,"경기도 고양시"))
-        mStudentList.add(studentData("서정민", 1984,"서울시 동대문구"))
-        mStudentList.add(studentData("이승민", 1994,"서울시 동대문구"))
-        mStudentList.add(studentData("이영종", 1991,"서울시 동대문구"))
-        mStudentList.add(studentData("이윤주", 1989,"서울시 동대문구"))
+        mStudentList.add(studentData("서정민", 1984,"경기도 광명시"))
+        mStudentList.add(studentData("이승민", 1994,"경기도 안양시"))
+        mStudentList.add(studentData("이영종", 1991,"경기도 고양시"))
+        mStudentList.add(studentData("이윤주", 1989,"서울시 강서구"))
         mStudentList.add(studentData("이진실", 1994,"서울시 동대문구"))
-        mStudentList.add(studentData("정동준", 1988,"서울시 동대문구"))
-        mStudentList.add(studentData("최재훈", 1988,"서울시 동대문구"))
+        mStudentList.add(studentData("정동준", 1992,"서울시 동대문구"))
+        mStudentList.add(studentData("최재훈", 1994,"서울시 동대문구"))
         mStudentList.add(studentData("이성노", 1988,"서울시 동대문구"))
+
+
+        mAdapter = StudentAdapter(this,R.layout.student_list_item,mStudentList)
+        studentListView.adapter = mAdapter
 
     }
 }
