8fd9721fdfc96794795fde12f68ef664e19ba734
이 영 종 <naki5087@naver.com>
2022-02-16 15:38:02 +0900

    완성된 row를 결과로 return 처리

--- app/src/main/java/com/nepplus/listview_20220216/adapters/StudentAdapter.kt
+++ app/src/main/java/com/nepplus/listview_20220216/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.nepplus.listview_20220216.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.nepplus.listview_20220216.R
 import com.nepplus.listview_20220216.datas.studentData
 
 class StudentAdapter(
@@ -9,4 +13,15 @@ class StudentAdapter(
     val resId: Int,
     val mList: ArrayList<studentData>
 ) : ArrayAdapter<studentData>(mContext,resId,mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow =  LayoutInflater.from(mContext).inflate(R.layout.student_list_item,null)
+        }
+        val row = tempRow!!
+
+        return row
+    }
 }
