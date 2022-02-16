ea80ff9019bb76be64bdd526d359f1cbcc4b5e3a
이 영 종 <naki5087@naver.com>
2022-02-16 15:15:42 +0900

    ArrayAdapter 상속을 받을때, 필요한 재료들을 자녀 클래스가 받아서, 부모에게 전달.

--- app/src/main/java/com/nepplus/listview_20220216/adapters/StudentAdapter.kt
+++ app/src/main/java/com/nepplus/listview_20220216/adapters/StudentAdapter.kt
@@ -1,4 +1,12 @@
 package com.nepplus.listview_20220216.adapters
 
-class StudentAdapterk {
+import android.content.Context
+import android.widget.ArrayAdapter
+import com.nepplus.listview_20220216.datas.studentData
+
+class StudentAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<studentData>
+) : ArrayAdapter<studentData>(mContext,resId,mList) {
 }
